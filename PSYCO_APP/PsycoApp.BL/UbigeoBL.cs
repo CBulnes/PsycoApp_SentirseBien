7de057cfc91c5ccd17392a1d72371754cf45e41a
CBulnes <kalbulnes@gmail.com>
2024-09-17 12:21:48 -0500

    Ubigeos

---
+++ PSYCO_APP/PsycoApp.BL/UbigeoBL.cs
@@ -0,0 +1,23 @@
+﻿using PsycoApp.DA;
+using PsycoApp.entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PsycoApp.BL
+{
+    public class UbigeoBL
+    {
+        private readonly UbigeoDA _ubigeoDA;
+
+        public UbigeoBL()
+        {
+            _ubigeoDA = new UbigeoDA();
+        }
+
+        public List<Ubigeo> ListarUbigeos()
+        {
+            return _ubigeoDA.ListarUbigeos();
+        }
+    }
+}
