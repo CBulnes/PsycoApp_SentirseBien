52e81d1c1945b28289ae168112559efed14aa92a
angelolag <77652602+angelolag@users.noreply.github.com>
2024-09-17 09:22:21 -0500

    Cambios de agregar administrador de pacientes

---
+++ PSYCO_APP/PsycoApp.site/wwwroot/src/js/sb-main.js
@@ -0,0 +1,39 @@
+﻿
+window.addEventListener("load", function (event) {
+
+    const btn_colapse = document.querySelector('.sb-aside-compress');
+    const abrir_pop = document.querySelector('.sb-aside-logoff');
+    const cerrar_pop1 = document.querySelector('.sb-alert-off-button-no');
+    const cerrar_pop2 = document.querySelector('.sb-alert-off-block-cross');
+
+    abrir_pop.addEventListener('click', () => {
+        document.querySelector('.sb-alert-off').style.display = 'flex';
+    });
+
+    cerrar_pop1.addEventListener('click', () => {
+        document.querySelector('.sb-alert-off').style.display = 'none';
+    });
+
+    cerrar_pop2.addEventListener('click', () => {
+        document.querySelector('.sb-alert-off').style.display = 'none';
+    });
+
+    btn_colapse.addEventListener('click', (e) => {
+
+        var aside_active = document.querySelector('.sb-aside');
+        var admin_active = document.querySelector('.sb-admninistrator');
+        var list_class = aside_active.classList[1];
+
+        if (list_class === 'displayed') {
+            aside_active.classList.remove("displayed");
+            admin_active.classList.remove("displayed");
+        }
+        else {
+            if (list_class !== 'displayed') {
+                aside_active.classList.add("displayed");
+                admin_active.classList.add("displayed");
+            }
+        }
+
+    });
+});
