2effe78cb551d285f9bc8cbe68a469ebf26446a3
JUANJO1996 <33628408+JUANJO1996@users.noreply.github.com>
2025-01-30 02:35:37 -0500

    Cambios filtros sede y psicologos

--- PSYCO_APP/PsycoApp.site/Controllers/RegistroCitasController.cs
+++ PSYCO_APP/PsycoApp.site/Controllers/RegistroCitasController.cs
@@ -59,6 +59,7 @@ namespace PsycoApp.site.Controllers
                 obj.id_psicologo = HttpContext.Session.GetInt32("id_psicologo");
                 obj.tipo_documento = HttpContext.Session.GetString("tipo_documento");
                 obj.num_documento = HttpContext.Session.GetString("num_documento");
+                obj.id_sede = HttpContext.Session.GetInt32("id_sede"); 
                 obj.vista = "ATENCION";
                 obj.call_center_invitado = Helper.GetCallCenterInvitado();
 
